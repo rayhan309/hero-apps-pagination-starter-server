89e3bfda9c1ddc5132317ddd7278bf69b72f0dcc
Abu Rayhan <ihaveawonderfull@gmail.com>
2025-12-04 16:14:33 -1200

    sort by size, download, retigns in DB

---
+++ index.js
@@ -0,0 +1,111 @@
+//Definition & imports
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+
+// Middlewares
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  console.log(
+    `⚡ ${req.method} - ${req.path} from ${
+      req.host
+    } at ⌛ ${new Date().toLocaleString()}`
+  );
+  next();
+});
+
+//ports & clients
+const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//listeners
+client
+  .connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Hero Apps Server listening ${port}`);
+      console.log(`Hero Apps Server Connected with DB`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//DB & collections
+const database = client.db("heroAppsDB");
+const appsCollection = database.collection("apps");
+
+//Apps Route
+
+app.get("/apps", async (req, res) => {
+  try {
+    const { limit = 0, skip = 0, sort = "size", order = "asc" } = req.query;
+    console.log({ sort, order });
+    const sortOrder = order === "asc" ? 1 : -1;
+    const apps = await appsCollection
+      .find()
+      .sort({ [sort]: sortOrder })
+      .limit(Number(limit))
+      .skip(Number(skip))
+      .project({ description: 0, rating: 0 })
+      .toArray();
+
+    const totaleAppCount = await appsCollection.countDocuments();
+
+    res.send({ apps, totaleAppCount });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/apps", async (req, res) => {
+  try {
+    const neaApp = req.body;
+    const apps = await appsCollection.insertOne(neaApp);
+    res.send(apps);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/apps/:id", async (req, res) => {
+  try {
+    const appId = req.params.id;
+
+    if (appId.length != 24) {
+      res.status(400).json({ error: "Invalid ID" });
+      return;
+    }
+    const query = new ObjectId(appId);
+    const app = await appsCollection.findOne({ _id: query });
+    res.json(app);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Basic routes
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Hero Apps Server" });
+});
+//404
+app.all(/.*/, (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: "API not found",
+  });
+});
